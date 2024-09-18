ea3febef00256483b5416d8df97e14c868789782
jang-seowoo <swswsw7054@gmail.com>
2024-09-18 18:45:12 +0900

    4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+'use client'
 
+import { collection, addDoc } from "firebase/firestore";
+import {firestore} from "./firebase/firestore";
 
 export default function Home() {
+
+  const onClickUpLoadButton = async () => {
+    await addDoc(collection(firestore, "users"),
+      {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815
+      })
+  }
+
   return (
-    <div><h1>ㅎㅇ</h1></div>
-  );
-}
+    <div>
+      <button onClick={onClickUpLoadButton}>Ada Lovelace 등록</button>
+    </div>
+  )}
